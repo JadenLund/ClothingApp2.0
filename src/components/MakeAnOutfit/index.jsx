e37bc685d5abb3db6c371e18ce9cbae2dd800570
JadenLund <lundjaden@gmail.com>
2023-04-01 00:02:58 -0500

    mvp of the showshirts and show pants form

--- src/components/MakeAnOutfit/index.jsx
+++ src/components/MakeAnOutfit/index.jsx
@@ -1,32 +1,41 @@
 import { useState } from "react"
 import { Button, Form } from "semantic-ui-react"
 export default function MakeAnOutfit() {
+    const [showShirtForm, setShowShirtForm] = useState(true)
+    const [showPantsForm, setShowPantsForm] = useState(true)
 
+    const handleShirtFormClick = () => {
+        setShowShirtForm(!showShirtForm);
+    }
+    const handlePantsFormClick = () => {
+        setShowPantsForm(!showPantsForm);
+    }
     return (
         <>
             <div className="add-shirt-container">
-                <Button >
+                <Button onClick={handleShirtFormClick}>
                     Add Shirt
                 </Button>
-                <Form>
+                {showShirtForm ? null : <Form>
                     <Form.Field width={4}>
                         <input placeholder='Image URL...' />
                         <input placeholder='Clothing Name...' />
                     </Form.Field>
                     <Button type='submit'>Submit</Button>
-                </Form>
+                </Form>}
+
             </div>
             <div className="add-pants-container">
-                <Button >
+                <Button onClick={handlePantsFormClick}>
                     Add Pants
                 </Button>
-                <Form>
+                {showPantsForm ? null : <Form>
                     <Form.Field width={4}>
                         <input placeholder='Image URL...' />
                         <input placeholder='Clothing Name...' />
                     </Form.Field>
                     <Button type='submit'>Submit</Button>
-                </Form>
+                </Form>}
             </div>
         </>
     )
