1bae1697952f661cc79782999d5ee010efe3d887
JadenLund <lundjaden@gmail.com>
2023-04-01 00:20:32 -0500

    adding in the Pants and Shirts jsx files

--- src/components/MakeAnOutfit/index.jsx
+++ src/components/MakeAnOutfit/index.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react"
-import { Button, Form } from "semantic-ui-react"
+import { Button, Form, Icon } from "semantic-ui-react"
 export default function MakeAnOutfit() {
     const [showShirtForm, setShowShirtForm] = useState(true)
     const [showPantsForm, setShowPantsForm] = useState(true)
@@ -12,30 +12,39 @@ export default function MakeAnOutfit() {
     }
     return (
         <>
-            <div className="add-shirt-container">
-                <Button onClick={handleShirtFormClick}>
-                    Add Shirt
-                </Button>
-                {showShirtForm ? null : <Form>
-                    <Form.Field width={4}>
-                        <input placeholder='Image URL...' />
-                        <input placeholder='Clothing Name...' />
-                    </Form.Field>
-                    <Button type='submit'>Submit</Button>
-                </Form>}
-
+            <div className="shirt-container">
+                <div className="add-shirt-container">
+                    <Button onClick={handleShirtFormClick}>
+                        Add Shirt
+                    </Button>
+                    {showShirtForm ? null : <Form>
+                        <Form.Field width={4}>
+                            <input placeholder='Image URL...' />
+                            <input placeholder='Clothing Name...' />
+                        </Form.Field>
+                        <Button type='submit'>Submit</Button>
+                    </Form>}
+                </div>
+                <Icon name='arrow left' />
+                <div>Shirts Image Container</div>
+                <Icon name='arrow right' />
             </div>
-            <div className="add-pants-container">
-                <Button onClick={handlePantsFormClick}>
-                    Add Pants
-                </Button>
-                {showPantsForm ? null : <Form>
-                    <Form.Field width={4}>
-                        <input placeholder='Image URL...' />
-                        <input placeholder='Clothing Name...' />
-                    </Form.Field>
-                    <Button type='submit'>Submit</Button>
-                </Form>}
+            <div className="pants-container">
+                <div className="add-pants-container">
+                    <Button onClick={handlePantsFormClick}>
+                        Add Pants
+                    </Button>
+                    {showPantsForm ? null : <Form>
+                        <Form.Field width={4}>
+                            <input placeholder='Image URL...' />
+                            <input placeholder='Clothing Name...' />
+                        </Form.Field>
+                        <Button type='submit'>Submit</Button>
+                    </Form>}
+                </div>
+                <Icon name='arrow left' />
+                <div>Pants Image Container</div>
+                <Icon name='arrow right' />
             </div>
         </>
     )
