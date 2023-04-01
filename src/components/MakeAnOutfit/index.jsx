74b9111813a6f02a87fe1212d4c8b6c8d27778d6
JadenLund <lundjaden@gmail.com>
2023-03-31 23:56:55 -0500

    adding in the forms for the add shirt and add pants

--- src/components/MakeAnOutfit/index.jsx
+++ src/components/MakeAnOutfit/index.jsx
@@ -1,6 +1,33 @@
+import { useState } from "react"
+import { Button, Form } from "semantic-ui-react"
 export default function MakeAnOutfit() {
+
     return (
         <>
+            <div className="add-shirt-container">
+                <Button >
+                    Add Shirt
+                </Button>
+                <Form>
+                    <Form.Field width={4}>
+                        <input placeholder='Image URL...' />
+                        <input placeholder='Clothing Name...' />
+                    </Form.Field>
+                    <Button type='submit'>Submit</Button>
+                </Form>
+            </div>
+            <div className="add-pants-container">
+                <Button >
+                    Add Pants
+                </Button>
+                <Form>
+                    <Form.Field width={4}>
+                        <input placeholder='Image URL...' />
+                        <input placeholder='Clothing Name...' />
+                    </Form.Field>
+                    <Button type='submit'>Submit</Button>
+                </Form>
+            </div>
         </>
     )
 }
