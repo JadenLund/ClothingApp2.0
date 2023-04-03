6b01ce0a940670660b8bf4e48cdc53331866314c
JadenLund <lundjaden@gmail.com>
2023-04-03 15:53:53 -0500

    adding in the horizontal scrolling bar

--- src/components/Home/index.jsx
+++ src/components/Home/index.jsx
@@ -1,15 +1,16 @@
 import banner from '../../assets/Images/Banner.jpeg'
 import { Icon, Image } from 'semantic-ui-react'
 import Styles from "../Home/styles"
-import goth from "../../assets/Images/ClothingStyles/goth.jpg"
+import Test from './test'
+
 export default function Home() {
     return (
         <>
 
             <Image src={banner} size='massive' centered />
-            <Icon name="angle left" />
             <Styles />
-            <Icon name="angle right" />
+            <Test />
+
         </>
     )
 }
