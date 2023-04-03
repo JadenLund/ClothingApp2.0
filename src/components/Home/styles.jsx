e4a79a1040dc48bcb68ed7ef3fb4ad66c92b381e
JadenLund <lundjaden@gmail.com>
2023-04-02 22:59:39 -0500

    adding in all of the clothing style images

--- src/components/Home/styles.jsx
+++ src/components/Home/styles.jsx
@@ -1,5 +1,12 @@
 ///this is where all of the styles will go
 
-export default function Styles() {
+export default function Styles({ image, header }) {
+    //add an area so that the image can be used as a prop and not eneded to be repeated
 
+    return (
+        <div>
+            <img src={image} />
+            <h2>{header}</h2>
+        </div>
+    )
 }
